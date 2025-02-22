d53daeeab3e72f0b2766bb9e6ce4b3b90916906d
Anna Randeberg <anna-randeberg@hotmail.com>
2025-02-22 11:42:12 +0100

    added hashed passwords in db

--- backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
+++ backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (name, email, password) VALUES
-                                              ('testuser1', 'test1@email.com', 'password123'),
-                                              ('testuser2', 'test2@email.com', 'password123');
+                                              ('testuser1', 'test1@email.com', '$2a$10$8YhZsKiu5q2wiWeSXzWiQuYT9L73GaaKzEd57QKcH27Abj5.VcO2e'),
+                                              ('testuser2', 'test2@email.com', '$2a$10$v2p3WB8Ee7Hs4.8JeCL01O1c9dD/burdPiPoYVpevJI6WIXrFzNRa');
