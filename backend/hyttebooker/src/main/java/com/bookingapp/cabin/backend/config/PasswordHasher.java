76e123280faf74da00ae189029f318fa4b4b2806
Anna Randeberg <anna-randeberg@hotmail.com>
2025-02-22 11:41:34 +0100

    commentet out PasswordHasher.java

--- backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/config/PasswordHasher.java
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/config/PasswordHasher.java
@@ -2,9 +2,12 @@ package com.bookingapp.cabin.backend.config;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+//brukes for å hashe passord, kan slettes etter at alle brukeren har fått et hashet passord
+/*
 public class PasswordHasher {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("password123"));
+        System.out.println(encoder.encode(""));
     }
 }
+*/
