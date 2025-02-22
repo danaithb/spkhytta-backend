cbd11e01facbc2fa54a3a5434fa64526bcc9e1cd
Anna Randeberg <anna-randeberg@hotmail.com>
2025-02-22 11:17:50 +0100

    Made a temporarily passwordhasher

---
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/config/PasswordHasher.java
@@ -0,0 +1,10 @@
+package com.bookingapp.cabin.backend.config;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordHasher {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("password123"));
+    }
+}
