160758a7bd2c41d8fc1fabd54181e3832fb16abe
Anna Randeberg <anna-randeberg@hotmail.com>
2025-02-27 14:26:38 +0100

    updated create table users

--- backend/hyttebooker/src/main/resources/db/migration/V1__create_users_table.sql
+++ backend/hyttebooker/src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,6 +1,6 @@
 create table if not exists users (
     user_id int auto_increment primary key,
-    name varchar(255) not null ,
     email varchar(255) unique not null,
+    name varchar(255),
     firebase_uid VARCHAR(255) UNIQUE
 );
