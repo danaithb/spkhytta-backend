9d24dc8844627f572973171ae14a1bd0bd2cf772
Anna Randeberg <anna-randeberg@hotmail.com>
2025-02-27 14:26:48 +0100

    updated insert into users

--- backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
+++ backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (name, email, password) VALUES
-                                              ('testuser1', 'test1@email.com', '$2a$10$8YhZsKiu5q2wiWeSXzWiQuYT9L73GaaKzEd57QKcH27Abj5.VcO2e'),
-                                              ('testuser2', 'test2@email.com', '$2a$10$v2p3WB8Ee7Hs4.8JeCL01O1c9dD/burdPiPoYVpevJI6WIXrFzNRa');
+INSERT INTO users (email, firebase_uid, name)
+SELECT 'testing4@test.no', '', 'hanna banana'
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'testing4@test.no');
