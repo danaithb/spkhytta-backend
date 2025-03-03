77fe04a5a3946cbade44eb9c9a3cdfadfca1089d
danaithadra <daha025@student.kristiania.no>
2025-03-03 22:53:49 +0100

    small changes

--- backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
+++ backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (name, email, firebase_uid) VALUES
-                                                  ('testuser1', 'test1@email.com', 'dummy_uid_1'),
-                                                  ('testuser2', 'test2@email.com', 'dummy_uid_2');
+                                                  ('testuser1', 'test1@email.com', 'dummy_firebase_uid_1'),
+                                                  ('testuser2', 'test2@email.com', 'dummy_firebase_uid_2');
