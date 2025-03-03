4b8a9d41704bb9775c5d812e13fac9f7096cbfbb
danaithadra <daha025@student.kristiania.no>
2025-03-03 22:14:02 +0100

    removed password from dummy users in flyway

--- backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
+++ backend/hyttebooker/src/main/resources/db/migration/V2__insert_dummy_users.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (name, email, password) VALUES
-                                              ('testuser1', 'test1@email.com', '$2a$10$8YhZsKiu5q2wiWeSXzWiQuYT9L73GaaKzEd57QKcH27Abj5.VcO2e'),
-                                              ('testuser2', 'test2@email.com', '$2a$10$v2p3WB8Ee7Hs4.8JeCL01O1c9dD/burdPiPoYVpevJI6WIXrFzNRa');
+INSERT INTO users (name, email, firebase_uid) VALUES
+                                                  ('testuser1', 'test1@email.com', 'dummy_uid_1'),
+                                                  ('testuser2', 'test2@email.com', 'dummy_uid_2');
