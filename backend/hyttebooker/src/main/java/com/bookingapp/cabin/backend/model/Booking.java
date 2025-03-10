c00297e868e09abd31e08929f58354acd7fe9fca
Anna Randeberg <anna-randeberg@hotmail.com>
2025-03-10 17:42:49 +0100

    added model for booking

---
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/model/Booking.java
@@ -0,0 +1,52 @@
+package com.bookingapp.cabin.backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "cabin_id", referencedColumnName = "cabin_id")
+    private Cabin cabin;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "queue_position")
+    private Integer queuePosition;
+
+
+    private String status;
+
+    public Booking(Users user, Cabin cabin, LocalDate startDate, LocalDate endDate, String status) {
+        this.user = user;
+        this.cabin = cabin;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.queuePosition = null;
+    }
+
+}
