02678068d4fa3043ff9393460a8fbeafba1e7904
danaithadra <daha025@student.kristiania.no>
2025-03-17 14:33:09 +0100

    added a simple hello world for localhost/

---
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.bookingapp.cabin.backend.controller;
+
+// klasse for lokaltesting
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")  // Setter base-path til rotnivå
+public class HelloController {
+
+    @GetMapping  // Når du besøker http://localhost:8080/
+    public String helloWorld() {
+        return "Hello world!";
+    }
+}
