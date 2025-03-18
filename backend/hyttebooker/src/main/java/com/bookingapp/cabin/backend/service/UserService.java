241c4736108e2df50fa4fdcc430c07ddf859f5ae
Anna Randeberg <anna-randeberg@hotmail.com>
2025-03-18 20:25:07 +0100

    added comments for better readability

--- backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/service/UserService.java
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/service/UserService.java
@@ -4,12 +4,12 @@ import com.bookingapp.cabin.backend.model.Users;
 import com.bookingapp.cabin.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+
+//Denne klassen er fikset
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
 
     @Autowired
@@ -17,17 +17,19 @@ public class UserService {
         this.userRepository = usersRepository;
     }
 
+    //Henter alle brukere
     public List<Users> getAllUsers() {
         return userRepository.findAll();
     }
 
+    //Henter en bruker basert på email
     public Optional<Users> getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
+    //Henter en bruker basert på firebase uid
     public Optional<Users> getUserByFirebaseUid(String firebaseUid) {
         return userRepository.findByFirebaseUid(firebaseUid);
     }
-
 
 }
