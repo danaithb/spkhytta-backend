c96cb8809c9125b1fac15794cea50f711fa3c1c3
Anna Randeberg <anna-randeberg@hotmail.com>
2025-03-18 20:17:21 +0100

    Updated BookingRepository with a comment and query fix

--- backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/repository/BookingRepository.java
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/repository/BookingRepository.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-
+//Repository for booking, håndterer databaseoperasjoner
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByCabin_CabinIdAndStatus(Long cabinId, String status);
     List<Booking> findByCabin_CabinIdAndStatusAndStartDateBetween(Long cabinId, String status, LocalDate start, LocalDate end);
     List<Booking> findByCabin_CabinIdAndStatusOrderByQueuePositionAsc(Long cabinId, String status);
+    List<Booking> findByCabin_CabinIdAndStatusAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
+            Long cabinId, String status, LocalDate endDate, LocalDate startDate);
 
 }
