75b7e546f79582c07ecf7af5db1a692e38ad9627
Anna Randeberg <anna-randeberg@hotmail.com>
2025-03-18 20:17:41 +0100

    Made a DTO for BookingRequest

---
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/dtos/BookingRequestDTO.java
@@ -0,0 +1,16 @@
+package com.bookingapp.cabin.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class BookingRequestDTO {
+    private Long cabinId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
