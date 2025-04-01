115f3237aecbdf717a633019268aa5a3bf9b3cf4
danaithadra <daha025@student.kristiania.no>
2025-04-01 11:22:02 +0200

    Add Firebase configuration with support for local and GCP environments

--- backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/config/FirebaseConfig.java
+++ backend/hyttebooker/src/main/java/com/bookingapp/cabin/backend/config/FirebaseConfig.java
@@ -12,7 +12,8 @@ import java.io.IOException;
 import java.io.InputStream;
 
 // Henter Firebase-nøkkelen fra Google Secret Manager i stedet for å lagre den som en fil
-/*
+//source:https://stackoverflow.com/questions/44185432/firebase-admin-sdk-with-java/47247539#47247539
+
 @Configuration
 public class FirebaseConfig {
 
@@ -51,29 +52,5 @@ public class FirebaseConfig {
             AccessSecretVersionResponse response = client.accessSecretVersion(secretName);
             return response.getPayload().getData().toStringUtf8();
         }
-    }*/
-
-
-//source:https://stackoverflow.com/questions/44185432/firebase-admin-sdk-with-java/47247539#47247539
-
-@Configuration
-public class FirebaseConfig {
-
-    @Bean
-    public FirebaseApp initializeFirebase() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()){
-            try(InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase-adminsdk.json")) {
-                if (serviceAccount == null) {
-                    throw new IOException("Kunne ikke finne firebase-adminsdk.json.");
-                }
-
-                FirebaseOptions options = new FirebaseOptions.Builder()
-                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                        .build();
-
-                return FirebaseApp.initializeApp(options);
-            }
-        }
-        return FirebaseApp.getInstance();
     }
 }
