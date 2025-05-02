9cd252e89f791babc1d039b643c478ee67552135
danaithb <daha025@student.kristiania.no>
2025-05-02 12:14:54 +0200

    comment change

--- src/main/java/com/bookingapp/cabin/backend/controller/FaviconController.java
+++ src/main/java/com/bookingapp/cabin/backend/controller/FaviconController.java
@@ -2,11 +2,12 @@ package com.bookingapp.cabin.backend.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
 
 public class FaviconController {
     @RequestMapping("favicon.ico")
     @ResponseBody
     void returnNoFavicon() {
-        // Gjør ingenting, bare returner 200 OK og ikk error
+        // for gcp, returner 200 OK og ikk error
     }
 }
