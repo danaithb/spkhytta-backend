f85d815e2a77be0af017ed150509cfbc9650c9ac
danaithb <daha025@student.kristiania.no>
2025-05-05 00:07:15 +0200

    testing

--- src/main/java/com/bookingapp/cabin/backend/CabinBookingAppApplication.java
+++ src/main/java/com/bookingapp/cabin/backend/CabinBookingAppApplication.java
@@ -2,6 +2,12 @@ package com.bookingapp.cabin.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CabinBookingAppApplication {
@@ -10,4 +16,19 @@ public class CabinBookingAppApplication {
 		SpringApplication.run(CabinBookingAppApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(List.of("https://spkhytta.web.app"));
+		config.setAllowedHeaders(List.of("*"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		System.out.println("✅ CORS FILTER FROM MAIN APP");
+
+		return new CorsFilter(source);
+	}
 }
